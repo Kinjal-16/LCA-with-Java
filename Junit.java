5ed3e5580d01a2f6485780f0ea598d9008238be8
Kinjal Bhattacharyya <bhattacharyyakinjal@gmail.com>
2021-10-13 21:39:20 +0100

    Initial Unit Test File

---
+++ Junit.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Junit {
+    @Test
+    void simpleAssertion(){
+        Assertions.assertEquals(1,1);
+
+    }
+}
