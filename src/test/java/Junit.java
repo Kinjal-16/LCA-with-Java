7dc449e0e54b34bd4865c6726f17fc7b86203846
Kinjal Bhattacharyya <bhattacharyyakinjal@gmail.com>
2021-10-14 20:30:06 +0100

    More test cases

--- src/test/java/Junit.java
+++ src/test/java/Junit.java
@@ -4,14 +4,65 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Junit {
+    int checkLCA1(int x1, int x2) {
+
+
+        LCA ob2 = new LCA();
+        Insertion ob = new Insertion();
+        int a[] = {15, 10, 25, 8, 12, 20, 30, 6, 9, 18, 22};
+        int len = a.length;
+        for (int i = 0; i < len; i++) {
+            ob.create(a[i]);
+
+        }
+        return (ob2.lca(ob.root, x1, x2));
+
+
+    }
+
+    int checkLCA2(int x1, int x2) {
+
+        LCA ob2 = new LCA();
+        Insertion ob = new Insertion();
+        int a[] = {3, 10, 16, 29};
+        int len = a.length;
+        for (int i = 0; i < len; i++) {
+            ob.create(a[i]);
+
+        }
+        return (ob2.lca(ob.root, x1, x2));
+    }
+
+    int checkLCA3(int x1, int x2) {
+
+        LCA ob2 = new LCA();
+        Insertion ob = new Insertion();
+        int a[] = {10,3,12,11,16,29};
+        int len = a.length;
+        for (int i = 0; i < len; i++) {
+            ob.create(a[i]);
+
+        }
+        return (ob2.lca(ob.root, x1, x2));
+    }
     @Test
-    void simpleAssertion(){
+    void simpleAssertion() {
         LCA ob = new LCA();
         Assertions.assertAll(
-                ()-> assertEquals(10,ob.checkLCA(6,12)),
-                ()-> assertEquals(10,ob.checkLCA(10,12)),
-                ()-> assertEquals(15,ob.checkLCA(20,6)),
-                ()-> assertEquals(20,ob.checkLCA(18,22)),
-                ()-> assertEquals(30,ob.checkLCA(30,30)));
+                () -> assertEquals(10, checkLCA1(6, 12)),
+                () -> assertEquals(10, checkLCA1(10, 12)),
+                () -> assertEquals(15, checkLCA1(20, 6)),
+                () -> assertEquals(20, checkLCA1(18, 22)),
+                () -> assertEquals(30, checkLCA1(30, 30)));
+
+        Assertions.assertAll(
+                () -> assertEquals(16, checkLCA2(16, 29)),
+                () -> assertEquals(3, checkLCA2(3, 3)));
+
+        Assertions.assertAll(
+                () -> assertEquals(12, checkLCA3(11, 29)),
+                () -> assertEquals(12, checkLCA3(11, 16)));
+
+
     }
 }
