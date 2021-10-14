12af6b7dc94dd28818ab49e333be9accd11cd4fb
Kinjal Bhattacharyya <bhattacharyyakinjal@gmail.com>
2021-10-14 05:04:27 +0100

    Changes made for accommodating Unit Testing

--- src/test/java/Junit.java
+++ src/test/java/Junit.java
@@ -7,11 +7,11 @@ public class Junit {
     @Test
     void simpleAssertion(){
         LCA ob = new LCA();
-        assertEquals(10,ob.checkLCA(6,12));
-        assertEquals(10,ob.checkLCA(10,12));
-        assertEquals(15,ob.checkLCA(20,6));
-        assertEquals(20,ob.checkLCA(18,22));
-        assertEquals(30,ob.checkLCA(30,30));
-
+        Assertions.assertAll(
+                ()-> assertEquals(10,ob.checkLCA(6,12)),
+                ()-> assertEquals(10,ob.checkLCA(10,12)),
+                ()-> assertEquals(15,ob.checkLCA(20,6)),
+                ()-> assertEquals(20,ob.checkLCA(18,22)),
+                ()-> assertEquals(30,ob.checkLCA(30,30)));
     }
 }
